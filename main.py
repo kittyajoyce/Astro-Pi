60b6a9625becf264644b19161aceef0c7b7eb0dc
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2021-01-25 12:35:51 +0000

    Update main.py
    For 'try:',  'except Exception as e:' doesn't work at the moment so neither try or except is needed at this piont in time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from logzero import logger, logfile
-from sense_hat import SenseHat
 from ephem import readtle, degree
 from picamera import PiCamera
 from datetime import datetime, timedelta
@@ -7,60 +6,58 @@ from time import sleep
 import random
 from pathlib import Path
 import csv
+import cv2 as cv
 
 dir_path = Path(__file__).parent.resolve()
 
 # Set a log file name
-logfile(dir_path + "/teamname.log")
+logfile(dir_path + /"jinro.log")
 
 # Latest TLE data for ISS location
 name = "ISS (ZARYA)"
 line1 = "1 25544U 98067A   20316.41516162  .00001589  00000+0  36499-4 0  9995"
 line2 = "2 25544  51.6454 339.9628 0001882  94.8340 265.2864 15.49409479254842"
-iss = readtle(name, line1, line2)
 
-# Set up Sense Hat
-sh = SenseHat()
+iss = readtle(name, line1, line2)
+iss.compute()
 
 # Set up camera
 cam = PiCamera()
+# Setting resolution V1 camera
 cam.resolution = (1296, 972)
 
+# CSV file
 def create_csv_file(data_file):
-    """Create a new CSV file and add the header row"""
+    # Create a new CSV file and add the header row
     with open(data_file, 'w') as f:
         writer = csv.writer(f)
-        header = ("Date/time", "Temperature", "Humidity")
+        header = ("Date/time", "Temperature")
         writer.writerow(header)
 
 def add_csv_data(data_file, data):
-    """Add a row of data to the data_file CSV"""
+    # Add a row of data to the data_file CSV
     with open(data_file, 'a') as f:
         writer = csv.writer(f)
-        writer.writerow(data)
+        writer.writer.writerow(data)
 
-def get_latlon():
-    """Return the current latitude and longitude, in degrees"""
-    iss.compute() # Get the lat/long values from ephem
-    return (iss.sublat / degree, iss.sublong / degree)
 
+# assigning variables convert
 def convert(angle):
-    """
-    Convert an ephem angle (degrees, minutes, seconds) to
-    an EXIF-appropriate representation (rationals)
-    e.g. '51:35:19.7' to '51/1,35/1,197/10'
-    Return a tuple containing a boolean and the converted angle,
-    with the boolean indicating if the angle is negative.
-    """
     degrees, minutes, seconds = (float(field) for field in str(angle).split(":"))
     exif_angle = f'{abs(degrees):.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
     return degrees < 0, exif_angle
 
-def capture(camera, image):
-    """Use `camera` to capture an `image` file with lat/long EXIF data."""
-    iss.compute() # Get the lat/long values from ephem
+# return current lat/long to degrees
+def get_latlon():
+    iss.compute() 
+    return (iss.sublat / degree, iss.sublong / degree)
 
-    # convert the latitude and longitude to EXIF-appropriate representations
+# capture image with lat/long EXIF tags
+def capture(camera, image):
+    # getting lat/long
+    iss.compute()
+
+    # convert lat/long
     south, exif_latitude = convert(iss.sublat)
     west, exif_longitude = convert(iss.sublong)
 
@@ -74,38 +71,59 @@ def capture(camera, image):
     camera.capture(image)
 
 
+# assigning variable data_file
+data_file = dir_path/"data.csv"
+
 # initialise the CSV file
-data_file = dir_path + "/data.csv"
 create_csv_file(data_file)
-# initialise the photo counter
+
+# initialise the photo_counter
 photo_counter = 1
-# record the start and current time
+
+# rassigning variable start_time
 start_time = datetime.now()
+
+#assigning variable now_time
 now_time = datetime.now()
+
+# assigning variable i
+i = 1
+
 # run a loop for (almost) three hours
 while (now_time < start_time + timedelta(minutes=178)):
-    try:
-        humidity = round(sh.humidity, 4)
-        temperature = round(sh.temperature, 4)
+    '''try:'''
+    
         # get latitude and longitude
-        latitude, longitude = get_latlon()
+        lat, long = get_latlon()
+        
         # Save the data to the file
         data = (
             datetime.now(),
             photo_counter,
-            humidity,
-            temperature,
-            latitude,
-            longitude
+            lat,
+            long
         )
+        
         add_csv_data(data_file, data)
-        # capture image
-        image_file = f"{dir_path}/photo_{photo_counter:03d}.jpg"
-        capture(cam, image_file)
-        logger.info(f"iteration {photo_counter}")
-        photo_counter += 1
-        sleep(30)
+        
+        # capture images
+        for i in range(4):
+            image_file = f"{dir_path}/photo_{photo_counter:03d}.jpg"
+            capture(cam, image_file)
+            
+            # update i
+            i += 1
+            
+            # update photo_counter
+            photo_counter += 1
+            
+            # update logfile
+            logger.info(f"iteration{photo_counter}")
+            
+        sleep(12)
+        
         # update the current time
         now_time = datetime.now()
-    except Exception as e:
-        logger.error('{}: {})'.format(e.__class__.__name__, e))
+        
+    '''except Exception as e:
+        logger.error('{}: {})'.format(e.__class__.__name__, e))'''
